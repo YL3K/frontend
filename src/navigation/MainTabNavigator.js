27afc168cf53493bafd8a57fbb0fc4257f432881
KWJ <81617589+pq5910@users.noreply.github.com>
2024-11-14 17:47:10 +0900

    feat: 탭 기능

--- src/navigation/MainTabNavigator.js
+++ src/navigation/MainTabNavigator.js
@@ -1,15 +1,87 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/MaterialIcons'
 import HomeScreen from '../screens/HomeScreen';
-import ProfileScreen from '../screens/ProfileScreen';
+import AnalysisScreen from '../screens/AnalysisScreen';
 
 const Tab = createBottomTabNavigator();
 
 function MainTabNavigator() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Profile" component={ProfileScreen} />
+    <Tab.Navigator
+    screenOptions={{
+      tabBarStyle: {
+        height: 66,
+        paddingBottom: 8,
+        paddingTop: 8,
+      },
+      tabBarLabelStyle: {
+        fontSize: 10,
+        color: '#495057',
+      },
+      headerShown: false,
+    }}
+  >
+      <Tab.Screen
+        name="홈"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ color, size}) => (
+            <Icon name="home" color='#495057' size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="금융상품"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="work" color='#495057' size={size} />
+          ),
+        }}
+        listeners={{
+          tabPress: (e) => {
+            e.preventDefault();
+          },
+        }}
+      />
+      <Tab.Screen
+        name="자산관리"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="bar-chart" color='#495057' size={size} />
+          ),
+        }}
+        listeners={{
+          tabPress: (e) => {
+            e.preventDefault();
+          },
+        }}
+      />
+      <Tab.Screen
+        name="혜택"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="redeem" color='#495057' size={size} />
+          ),
+        }}
+        listeners={{
+          tabPress: (e) => {
+            e.preventDefault();
+          },
+        }}
+      />
+      <Tab.Screen
+        name="상담데이터"
+        component={AnalysisScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="analytics" color='#495057' size={size} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
