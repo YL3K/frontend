76bbe8ae79dd4f9890db18bc3ac97edb0f04e5b7
다영 <86220363+imdayoung@users.noreply.github.com>
2024-11-17 03:46:29 +0900

    feat: 상담 종료 시 API 호출 및 로딩 완료 시 화면 변경

--- src/screens/Counsel/EndSessionScreen.js
+++ src/screens/Counsel/EndSessionScreen.js
@@ -1,11 +1,39 @@
 // 상담 종료 페이지
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, Image, StyleSheet } from 'react-native';
+import axios from 'axios';
 import MainButton from '../../components/button/MainButton';
 import SummaryProgress from '../../components/SummaryProgress';
 import SummaryComplete from '../../components/SummaryComplete';
 
-function EndSessionScreen() {
+function EndSessionScreen({ navigation }) {
+  const [isLoading, setIsLoading] = useState(true);
+  const roomId = 1; // mock data
+  const counselerName = "김재동"; // mock data
+
+  useEffect(() => {
+    const requestStt = async (roomId) => {
+
+      const formData = new FormData();
+      formData.append("roomId", roomId);
+      console.log(formData.getParts());
+
+      try {
+          await axios.post('http://10.0.2.2:8080/api/v1/stt/test/long', formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          }
+        });
+      } catch (error) {
+        console.error("STT, 요약 중 오류 발생: ", error);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    requestStt(roomId);
+  }, []);
+
   return (
     <View style={styles.container}>
       <View style={styles.content}>
@@ -14,33 +42,42 @@ function EndSessionScreen() {
           style={styles.profileImage}
         />
         <Text style={styles.nameContainer}>
-          <Text style={styles.nameText}>김재동 </Text>
+          <Text style={styles.nameText}>{counselerName} </Text>
           <Text>상담사</Text>
         </Text>
         <Text style={styles.statusText}>상담이 종료되었습니다.</Text>
         <View style={styles.summaryContainer}>
-          {/* <SummaryProgress /> */}
-          <SummaryComplete />
+          {isLoading ? (
+            <SummaryProgress />
+          ) : (
+            <SummaryComplete />
+          )}
         </View>
       </View>
-      {/* <View>
-        <MainButton
-          title="홈 화면으로 이동"
-          style={styles.homeButton}
-        >  
-        </MainButton>
-      </View> */}
-      <View style={styles.buttonContainer}>
-        <MainButton
-          title="홈 화면"
-          backgroundColor="#FFFFFF"
-          style={styles.button}
-        />
-        <MainButton
-          title="상담 요약 이동"
-          style={styles.button}
-        />
-      </View>
+      {isLoading ? (
+        <View>
+          <MainButton
+            title="홈 화면으로 이동"
+            style={styles.homeButton}
+            onPress={() => navigation.navigate('Main')}
+          >  
+          </MainButton>
+        </View>
+      ) : (
+        <View style={styles.buttonContainer}>
+            <MainButton
+            title="홈 화면"
+            backgroundColor="#FFFFFF"
+            style={styles.button}
+            onPress={() => navigation.navigate('Main')}
+          />
+          <MainButton
+            title="상담 요약 이동"
+            style={styles.button}
+            onPress={() => navigation.navigate('Record')}
+          />
+        </View>
+      )}
     </View>
   );
 }
