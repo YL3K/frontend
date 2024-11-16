d6ba83464b542d9411ba02967f0864748f463af0
KWJ <81617589+pq5910@users.noreply.github.com>
2024-11-17 02:43:50 +0900

    style: 탭 사이즈 2로 수정

--- src/components/analysis/AgePieChart.js
+++ src/components/analysis/AgePieChart.js
@@ -5,36 +5,36 @@ import { PieChart } from 'react-native-chart-kit';
 const screenWidth = Dimensions.get('window').width;
 
 function AgePieChart({data}){
-    return (
-        <PieChart
-            data={data}
-            width={screenWidth - 80}
-            height={220}
-            chartConfig={{
-                color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
-                labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
-            }}
-            accessor="population"
-            backgroundColor="transparent"
-            paddingLeft="15"
-            absolute
-        />
-    );
+  return (
+    <PieChart
+      data={data}
+      width={screenWidth - 80}
+      height={220}
+      chartConfig={{
+        color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+        labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+      }}
+      accessor="population"
+      backgroundColor="transparent"
+      paddingLeft="15"
+      absolute
+    />
+  );
 }
 
 const styles = {
-    chartContainer: {
-        borderWidth: 1,
-        borderColor: 'gray',
-        borderRadius: 15,
-        marginBottom: 10,
-        padding: 15,
-    },
-    title: {
-        fontSize: 18,
-        fontWeight: 'bold',
-        marginVertical: 10,
-    },
+  chartContainer: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 15,
+    marginBottom: 10,
+    padding: 15,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginVertical: 10,
+  },
 };
 
 export default AgePieChart;
