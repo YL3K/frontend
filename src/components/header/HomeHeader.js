dddff9b8c7b308f6abf3c84b8aaca5e214a0ff05
find11570 <find11570@naver.com>
2024-11-20 20:25:15 +0900

    feat: 화상상담 대기열 연동 구현

--- src/components/header/HomeHeader.js
+++ src/components/header/HomeHeader.js
@@ -1,15 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, TouchableOpacity, StyleSheet, Text, Modal, TouchableWithoutFeedback, ActivityIndicator } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import WaitingModal from '../modal/WaitingModal';
+import { useSelector } from 'react-redux';
+import axios from 'axios';
+import ReusableModal from '../modal/ReusableModal';
+import ReusableTwoBtnModal from '../modal/ReusableTwoBtnModal';
 
 const HomeHeader = () => {
+  const navigation = useNavigation();
+
+  // 1. HomeHeader 관련
   const [isCounselingStarted, setIsCounselingStarted] = useState(false); // 상담 시작 여부 상태 관리
   const [modalVisible, setModalVisible] = useState(false); // 모달 표시 여부 상태 관리
   const [waitingModalVisible, setWaitingModalVisible] = useState(false); // 대기열 모달
-
-  const navigation = useNavigation();
+  const [connectModalVisiable, setConnectModalVisiable] = useState(false); // 대기열 입장 모달
+  const [waitingCancelModalVisiable, setWaitingCancelModalVisiable] = useState(false); // 대기열 취소 안내 모달
 
   // 상담 버튼 클릭 시 처리
   const handleCounseling = () => {
@@ -22,18 +29,15 @@ const HomeHeader = () => {
   };
 
   // 상담 시작 버튼 클릭 시 처리
-  const handleStartCounseling = () => {
-    setIsCounselingStarted(true); // 상담 시작 상태로 변경
-    setModalVisible(false); // 상담 시작 후 모달 닫기
-    setWaitingModalVisible(true);
+  const handleStartCounseling = async () => {
+    await handleAddCustomer();
     console.log("상담 시작");
     // navigation.navigate('Counsel', {screen:'CounselorWaitingScreen'});
   };
 
   // 상담 취소 버튼 클릭 시 처리
-  const handleCencelCounseling = () => {
-    setIsCounselingStarted(false); // 상담 취소 상태로 변경
-    setModalVisible(false); // 상담 종료 후 모달 닫기
+  const handleCencelCounseling = async () => {
+    await handleDeleteCustomer();
     console.log("상담 취소");
   };
 
@@ -45,7 +49,138 @@ const HomeHeader = () => {
 
   const handleClose = () => {
     setWaitingModalVisible(false);
-    console.log('창 닫기');
+    console.log('waiting 창 닫기');
+  };
+
+  const handleCancelModalClose = () => {
+    setWaitingCancelModalVisiable(false);
+    console.log('cancel 창 닫기');
+  }
+
+  const handleConnectJoin = () => {
+    // 상담방으로 연결
+    setConnectModalVisiable(false);
+    console.log('상담 연결');
+    navigation.navigate('Counsel', {screen:'VideoConsult'});
+  };
+
+  const handleConnectCancel = () => {
+    setConnectModalVisiable(false);
+    // 상담 연결이 취소되었습니다. 모달 띄우기
+    setWaitingCancelModalVisiable(true);
+    // 상담 연결 취소
+    console.log('상담 취소');
+    handleCancel();
+  };
+
+
+  // 2. 웹소켓 관련
+  const user = useSelector((state) => state.user.user);
+  const userId = useSelector((state) => state.user.user.userId);
+  const userName = useSelector((state) => state.user.user.userName);
+  const [userSessionId, setUserSessionId] = useState('');
+  const [waitingQueue, setWaitingQueue] = useState([]); // 대기열 상태
+  const [websocket, setWebsocket] = useState(null); // WebSocket 객체
+
+  // 컴포넌트가 마운트될 때 WebSocket 연결 설정
+  useEffect(() => {
+    const socket = new WebSocket('ws://10.0.2.2:8080/ws'); // 서버 WebSocket URL
+
+    // WebSocket 연결이 열렸을 때 처리
+    socket.onopen = () => {
+      console.log('WebSocket connected');
+    };
+
+    // 서버에서 메시지를 받을 때 처리
+    socket.onmessage = (event) => {
+      const message = JSON.parse(event.data);
+      console.log('Received message:', message);
+      // 대기열 상태가 변경되었을 때 상태 업데이트
+      if (message.userSessionId) {
+        console.log("고객님의 유저아이디는 ", message.userSessionId, " 입니다.");
+        setUserSessionId(message.userSessionId);
+      }
+      if (message.type === 'queue_update') {
+        console.log("대기열 업데이트");
+        const queueArray = JSON.parse(message.queue);
+        setWaitingQueue(queueArray); // 대기열 상태를 화면에 업데이트
+      }
+      if (message.type === 'queue_assign') {
+        // 모든 변수 초기화
+        setWaitingModalVisible(false);
+        
+        // 이동 모달 띄우기
+        // 예 누르면 navigation.navigate('Counsel', {screen:'VideoConsult'});
+        // 아니오 누르면 새로운 알람 뜨면서 상담이 취소되었습니다.
+        setConnectModalVisiable(true);
+      }
+    };
+
+    // WebSocket 연결이 닫혔을 때 처리
+    socket.onclose = () => {
+      console.log('WebSocket disconnected');
+    };
+
+    // 컴포넌트가 언마운트될 때 WebSocket 연결 종료
+    return () => {
+      socket.close();
+    };
+  }, []);
+
+  useEffect(() => {
+    // waitingQueue 상태가 업데이트될 때 로그 출력
+    console.log("Updated waitingQueue:", waitingQueue);
+  }, [waitingQueue]);
+  
+  const handleAddCustomer = async () => {
+    if (!userSessionId) {
+      console.error("userSessionId is not available yet.");
+      return;
+    }
+    await addCustomerAPI();
+  };
+
+  const addCustomerAPI = async () => {
+    try {
+      const response = await axios.post(`http://10.0.2.2:8080/api/v1/counsel/queue?sessionId=${userSessionId}`, {}, {
+        headers: {
+          Authorization: `Bearer ${user.accessToken}`  // 토큰을 Authorization 헤더에 넣기
+        }
+    });
+
+      console.log(response.data);
+      setIsCounselingStarted(true); // 상담 시작 상태로 변경
+      setModalVisible(false); // 상담 시작 후 모달 닫기
+      setWaitingModalVisible(true);
+
+    } catch (error) {
+      console.error(error);
+    }  
+  };
+
+  const handleDeleteCustomer = async () => {
+    if (!isCounselingStarted) {
+      console.error("대기열에 존재하지 않습니다.");
+      return;
+    }
+    await deleteCustomerAPI();
+  };
+
+  const deleteCustomerAPI = async () => {
+    try {
+      const response = await axios.delete(`http://10.0.2.2:8080/api/v1/counsel/queue?userId=${userId}`, {
+        headers: {
+          Authorization: `Bearer ${user.accessToken}`  // 토큰을 Authorization 헤더에 넣기
+        }
+    });
+
+      console.log(response.data);
+      setIsCounselingStarted(false); // 상담 취소 상태로 변경
+      setModalVisible(false); // 상담 종료 후 모달 닫기
+
+    } catch (error) {
+      console.error(error);
+    }  
   };
 
   return (
@@ -112,6 +247,32 @@ const HomeHeader = () => {
           isVisible={waitingModalVisible}
           onCancel={handleCancel}
           onClose={handleClose}
+          waitingQueue={waitingQueue}
+          user={user}
+        />
+      )}
+
+      {/* 화상상담 연결 확인 모달 */}
+      {connectModalVisiable && (
+        <ReusableTwoBtnModal
+          isVisible={connectModalVisiable}
+          title="화상 상담 시작"
+          content={`${userName} 고객님의 상담 순서가 되었습니다.<br/>상담방에 입장하시겠습니까?`}
+          onBtnText1="거절"
+          onBtnAction1={handleConnectCancel}
+          onBtnText2="수락"
+          onBtnAction2={handleConnectJoin}
+          onClose={handleConnectCancel}
+        />
+      )}
+
+      {/* 화상상담 취소 모달 */}
+      {waitingCancelModalVisiable && (
+        <ReusableModal
+          isVisible={waitingCancelModalVisiable}
+          onClose={handleCancelModalClose}
+          title="화상상담 취소"
+          content="화상상담이 취소되였습니다."
         />
       )}
     </View>
